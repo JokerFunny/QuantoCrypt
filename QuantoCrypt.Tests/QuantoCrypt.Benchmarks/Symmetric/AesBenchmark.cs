b9c782647b23f04afa94b370ef636f3c6ba9d5cb
Danylo <31706307+JokerFunny@users.noreply.github.com>
2023-04-12 00:38:39 +0300

    - Update AesBenchmark.cs + new results.

--- QuantoCrypt.Tests/QuantoCrypt.Benchmarks/Symmetric/AesBenchmark.cs
+++ QuantoCrypt.Tests/QuantoCrypt.Benchmarks/Symmetric/AesBenchmark.cs
@@ -1,10 +1,9 @@
 ﻿using BenchmarkDotNet.Attributes;
 using FluentAssertions;
-using QuantoCrypt.Infrastructure.Common;
+using Org.BouncyCastle.Security;
 using QuantoCrypt.Infrastructure.Common.BlockCipher;
 using QuantoCrypt.Infrastructure.Common.Parameters;
 using QuantoCrypt.Internal.Symmetric;
-using System.Text;
 
 namespace QuantoCrypt.Benchmarks.Symmetric
 {
@@ -13,129 +12,81 @@ namespace QuantoCrypt.Benchmarks.Symmetric
     {
         //public static byte[] FileContent = File.ReadAllBytes("TestData\\input.txt");
 
-        public static byte[] RandomInput = new SecureRandom().GenerateSeed(2097152);
-        public static byte[] Key = Encoding.UTF8.GetBytes("b14ca5898a4e4133bbce2ea2315a1916");
+        //public static byte[] RandomInput = new SecureRandom().GenerateSeed(2097152);
+        //public static byte[] Key = Encoding.UTF8.GetBytes("b14ca5898a4e4133bbce2ea2315a1916");
 
         [Benchmark(Baseline = true)]
-        [Arguments(32)]
-        [Arguments(64)]
-        [Arguments(100)] // add incorrect message size.
-        [Arguments(128)]
-        [Arguments(256)]
-        [Arguments(512)]
-        [Arguments(1024)]
-        [Arguments(2048)]
-        [Arguments(4096)]
-        [Arguments(8192)]
-        [Arguments(16384)]
-        [Arguments(32768)]
-        [Arguments(65536)]
-        [Arguments(131072)]
-        [Arguments(262144)]
-        [Arguments(524288)]
-        [Arguments(1048576)]
-        [Arguments(2097152)]
-        //[Arguments(int.MaxValue)]
-        public void AesGcmAlgorithmExecutor(int symbolsToProceed)
+        [ArgumentsSource(nameof(SymmetricAlgorithmInputParams))]
+        public void AesGcmAlgorithmExecutor(byte[] key, byte[] textToProceed)
         {
-            /*byte[] textToProceed = FileContent.Length < symbolsToProceed 
-                ? FileContent[0..symbolsToProceed] 
-                : FileContent;*/
-
-            byte[] textToProceed = RandomInput[0..symbolsToProceed];
-
-            AesGcmAlgorithm service = new AesGcmAlgorithm(Key);
+            AesGcmAlgorithm service = new AesGcmAlgorithm(key);
 
             var encrypted = service.Encrypt(textToProceed);
 
             var decrypted = service.Decrypt(encrypted);
 
-            textToProceed.Should().BeEquivalentTo(decrypted);
+            //textToProceed.Should().BeEquivalentTo(decrypted);
         }
 
         [Benchmark]
-        [Arguments(32)]
-        [Arguments(64)]
-        [Arguments(100)] // add incorrect message size.
-        [Arguments(128)]
-        [Arguments(256)]
-        [Arguments(512)]
-        [Arguments(1024)]
-        [Arguments(2048)]
-        [Arguments(4096)]
-        [Arguments(8192)]
-        [Arguments(16384)]
-        [Arguments(32768)]
-        [Arguments(65536)]
-        [Arguments(131072)]
-        [Arguments(262144)]
-        [Arguments(524288)]
-        [Arguments(1048576)]
-        [Arguments(2097152)]
-        //[Arguments(int.MaxValue)]
-        public void AesAlgorithmExecutor(int symbolsToProceed)
+        [ArgumentsSource(nameof(SymmetricAlgorithmInputParams))]
+        public void AesAlgorithmExecutor(byte[] key, byte[] textToProceed)
         {
-            /*byte[] textToProceed = FileContent.Length < symbolsToProceed 
-                ? FileContent[0..symbolsToProceed] 
-                : FileContent;*/
-
-            byte[] textToProceed = RandomInput[0..symbolsToProceed];
-
-            AesAlgorithm service = new AesAlgorithm(Key);
+            AesAlgorithm service = new AesAlgorithm(key);
 
             var encrypted = service.Encrypt(textToProceed);
 
             var decrypted = service.Decrypt(encrypted);
 
-            if (decrypted.Length != textToProceed.Length)
+            /*if (decrypted.Length != textToProceed.Length)
                 textToProceed.Should().BeEquivalentTo(decrypted[..textToProceed.Length]);
             else
-                textToProceed.Should().BeEquivalentTo(decrypted);
+                textToProceed.Should().BeEquivalentTo(decrypted);*/
         }
 
         [Benchmark]
-        [Arguments(32)]
-        [Arguments(64)]
-        [Arguments(128)]
-        [Arguments(256)]
-        [Arguments(512)]
-        [Arguments(1024)]
-        [Arguments(2048)]
-        [Arguments(4096)]
-        [Arguments(8192)]
-        [Arguments(16384)]
-        [Arguments(32768)]
-        [Arguments(65536)]
-        [Arguments(131072)]
-        [Arguments(262144)]
-        [Arguments(524288)]
-        [Arguments(1048576)]
-        [Arguments(2097152)]
-        public void Aes_X86AlgorithmExecutor(int symbolsToProceed)
+        [ArgumentsSource(nameof(SymmetricAlgorithmInputParams))]
+        public void Aes_X86AlgorithmExecutor(byte[] key, byte[] textToProceed)
         {
-            byte[] textToProceed = RandomInput[0..symbolsToProceed];
-
             var engine = new AesEngine_X86();
-            ICipherParameter keyParams = new KeyParameter(Key);
+            ICipherParameter keyParams = new KeyParameter(key);
 
             BufferedBlockCipher cipher = new BufferedBlockCipher(engine);
 
             cipher.Init(true, keyParams);
 
-            byte[] encoded = new byte[symbolsToProceed];
+            byte[] encoded = new byte[textToProceed.Length];
 
-            int len1 = cipher.ProcessBytes(textToProceed, 0, symbolsToProceed, encoded, 0);
+            int len1 = cipher.ProcessBytes(textToProceed, 0, textToProceed.Length, encoded, 0);
 
             cipher.DoFinal(encoded, len1);
 
             cipher.Init(false, keyParams);
 
-            byte[] decoded = new byte[symbolsToProceed];
+            byte[] decoded = new byte[textToProceed.Length];
             int len2 = cipher.ProcessBytes(encoded, 0, encoded.Length, decoded, 0);
 
             cipher.DoFinal(decoded, len2);
 
-            textToProceed.Should().BeEquivalentTo(decoded);
+            //textToProceed.Should().BeEquivalentTo(decoded);
+        }
+
+        public IEnumerable<object[]> SymmetricAlgorithmInputParams()
+        {
+            SecureRandom secureRandom = new SecureRandom();
+            byte[] fileContent = File.ReadAllBytes("TestData\\input.txt");
+            byte[] key = secureRandom.GenerateSeed(32);
+
+            // check the message size which is not alligned with the block_size value.
+            yield return new object[] { key, fileContent[0..100] };
+
+            for (int i = 32; i < fileContent.Length; i *= 2) 
+            {
+                if (i > fileContent.Length)
+                    yield return new object[] { key, fileContent };
+                else
+                    yield return new object[] { key, fileContent[0..i] };
+            }
         }
     }
 }
